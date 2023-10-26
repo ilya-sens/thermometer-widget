b8e7588397ca5d0aa4ad9eb6de33df865788e5dc
Ilya Ananyev <ilyaananyev@gmail.com>
2023-10-26 15:14:14 +0200

    feat: thermometer styles

--- src/app/thermometer/thermometer.component.ts
+++ src/app/thermometer/thermometer.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'thermometer',
   templateUrl: './thermometer.component.html',
+  styleUrls: ['thermometer.component.scss'],
   standalone: true
 })
 export class ThermometerComponent {
-
 }
