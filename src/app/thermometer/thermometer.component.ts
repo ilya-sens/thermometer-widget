3425ff708371e8189dc7bb867df5ebe945af336d
Ilya Ananyev <ilyaananyev@gmail.com>
2023-10-26 16:05:08 +0200

    feat: added arrow head rotation

--- src/app/thermometer/thermometer.component.ts
+++ src/app/thermometer/thermometer.component.ts
@@ -7,7 +7,41 @@ import { Component, Input } from '@angular/core';
   standalone: true
 })
 export class ThermometerComponent {
-  @Input() minTemperature = 0;
-  @Input() maxTemperature = 0;
-  @Input() curTemperature = 0;
+  @Input()
+  get minTemperature() {
+    return this._minTemperature;
+  }
+  set minTemperature(minTemperature: number) {
+    this._minTemperature = minTemperature;
+    this.calculateArrowHeadRotation();
+  }
+  private _minTemperature = 0;
+
+  @Input()
+  get maxTemperature() {
+    return this._maxTemperature;
+  }
+  set maxTemperature(maxTemperature: number) {
+    this._maxTemperature = maxTemperature;
+    this.calculateArrowHeadRotation();
+  }
+  private _maxTemperature = 0;
+
+  @Input()
+  get curTemperature() {
+    return this._curTemperature;
+  }
+  set curTemperature(curTemperature: number) {
+    this._curTemperature = curTemperature;
+    this.calculateArrowHeadRotation();
+  }
+  private _curTemperature = 0;
+
+  private calculateArrowHeadRotation() {
+    this.arrowHeadRotation = 270 * this.curTemperature / (this.maxTemperature - this.minTemperature);
+  }
+
+  arrowHeadRotation = 0;
+
+
 }
