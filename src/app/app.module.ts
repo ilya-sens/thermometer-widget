aefc0b55cda43b2674f05c44832f2765dc63a4f9
Ilya Ananyev <ilyaananyev@gmail.com>
2023-10-26 13:11:27 +0200

    feat: rm generated code, added thermometer.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ThermometerComponent } from './thermometer/thermometer.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ThermometerComponent
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
